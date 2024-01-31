cb858035724a0a7add4d164a3afeb7b77046d19f
max rubel <maxrubel1@gmail.com>
2024-01-30 18:31:53 -0600

    setup working

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ function Home() {
   return (
     <div className="text-center my-4">
       <Link href="/book/new" passHref>
-        <Button>Add A Book</Button>
+        <Button>ADD A TRIP</Button>
       </Link>
       <div className="d-flex flex-wrap">
         {/* TODO: map over books here using BookCard component */}
